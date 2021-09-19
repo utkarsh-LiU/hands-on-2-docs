a0032b2c5703b48f7e6455dcd48b45cc742fe9c1
Rickard Armiento <rickard-gpg@armiento.net>
2021-09-20 01:02:51 +0200

    Initial commit

---
+++ homonucleardiatomicmolecule.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 Hands-on-2-docs developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+This file defines the HomonuclearDiatomicMolecule class closely inspired by one
+of the ASE example at: https://wiki.fysik.dtu.dk/ase/tutorials/atomization.html
+
+It also provides an example function to show how to use the
+HomonuclearDiatomicMolecule class.
+"""
+
+from ase import Atoms
+from ase.calculators.emt import EMT
+
+
+class HomonuclearDiatomicMolecule:
+    """A HomonuclearDiatomicMolecule is a linear molecule that consists of two
+    atoms of the same element.
+
+    A homonuclear diatomic molecule is fully defined by the element and distance
+    between the two atoms.
+
+    The class provides a number of methods to calculate various energy
+    expressions for this type of molecule using the ASE EMT energy calculator.
+    """
+
+    def __init__(self, element_symbol, distance):
+        """Create a HomonuclearDiatomicMolecule
+
+        Args:
+            element_symbol (string): The chemical element symbol for the element
+                that the molecule consists of.
+            distance (float): The distance between the two atoms.
+        """
+        self.atom = Atoms(element_symbol)
+        self.molecule = Atoms(
+            "2" + element_symbol, [(0.0, 0.0, 0.0), (0.0, 0.0, distance)]
+        )
+
+    def atom_energy(self):
+        """Calculate the energy of one separated atom of the element the
+        homonuclear diatomic molecule consists of using the ASE EMT energy
+        calculator.
+
+        Returns:
+            float: the energy of the atom in eV
+        """
+        self.atom.calc = EMT()
+        return self.atom.get_potential_energy()
+
+    def molecule_energy(self):
+        """Calculate the energy of the homonuclear diatomic molecule using
+        the ASE EMT energy calculator.
+
+        Returns:
+            float: the energy of the molecule in eV
+        """
+        self.molecule.calc = EMT()
+        return self.molecule.get_potential_energy()
+
+    def atomization_energy(self):
+        """Calculate the atomization energy of the homonuclear diatomic
+        molecule using the ASE EMT energy calculator.
+
+        The atomization energy is the energy it takes to break apart a
+        molecule into separate atoms. It is calculated as: the energy
+        of the individual atoms - the energy of the molecule.
+
+        Returns:
+            float: the atomization energy of the molecule in eV
+        """
+
+        e_atom = self.atom_energy()
+        e_molecule = self.molecule_energy()
+
+        return (2 * e_atom) - e_molecule
+
+
+def analyze_N2():
+
+    N2 = HomonuclearDiatomicMolecule("N", 1.1)
+
+    print("Nitrogen atom energy: %5.2f eV" % N2.atom_energy())
+    print("Nitrogen molecule energy: %5.2f eV" % N2.molecule_energy())
+    print("Atomization energy: %5.2f eV" % N2.atomization_energy())
