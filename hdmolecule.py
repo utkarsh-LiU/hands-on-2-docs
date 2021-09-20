c69365ed8b818d4e5a0fd95c5e99a81768b4007f
Rickard Armiento <rickard-gpg@armiento.net>
2021-09-20 02:00:57 +0200

    Update copyright headers to fit exercise

--- hdmolecule.py
+++ hdmolecule.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2021 Hands-on-2-docs developers
+# Copyright 2021 MDMolecule developers
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
